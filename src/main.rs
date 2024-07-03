38fc9f7238eb822655dc5b73377d5fe413c8a164
wlemalin <124249669+wlemalin@users.noreply.github.com>
2024-07-03 15:39:13 +0000

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use serde::{Deserialize, Serialize};
 use serde_json;
 use aes_gcm::{Aes256Gcm, Key, Nonce};
 use aes_gcm::aead::{Aead, NewAead};
+use zeroize::Zeroizing;
 
 const PASSWORD_FILE: &str = "passwords.enc";
 const NONCE: &[u8; 12] = b"unique nonce"; // This nonce should be unique per file
@@ -23,15 +24,15 @@ impl PasswordManager {
         }
     }
 
-    fn add_password(&mut self, name: String, password: String, master_password: &str) {
-        let extended_key = extend_key(master_password, 32);
+    fn add_password(&mut self, name: String, password: Zeroizing<String>, master_password: &Zeroizing<String>) {
+        let extended_key = extend_key(&master_password, 32);
         let encrypted_password = encrypt_data(password, &extended_key);
         self.passwords.insert(name, encrypted_password);
         self.save_passwords(master_password);
     }
 
-    fn get_password(&self, name: &str, master_password: &str) {
-        let extended_key = extend_key(master_password, 32);
+    fn get_password(&self, name: &str, master_password: &Zeroizing<String>) {
+        let extended_key = extend_key(&master_password, 32);
         if let Some(encrypted_password) = self.passwords.get(name) {
             match decrypt_data(encrypted_password.clone(), &extended_key) {
                 Ok(password) => println!("Mot de passe pour {}: {}", name, password),
@@ -49,23 +50,28 @@ impl PasswordManager {
         }
     }
 
-    fn save_passwords(&self, master_password: &str) {
-        let extended_key = extend_key(master_password, 32);
-        let serialized = serde_json::to_string(&self).unwrap();
-        let encrypted_data = encrypt_data(serialized, &extended_key);
-        let mut file = OpenOptions::new().write(true).create(true).truncate(true).open(PASSWORD_FILE).unwrap();
-        file.write_all(encrypted_data.as_bytes()).unwrap();
+    fn save_passwords(&self, master_password: &Zeroizing<String>) {
+        let extended_key = extend_key(&master_password, 32);
+        let serialized = serde_json::to_string(&self).expect("Erreur lors de la sérialisation des données.");
+        let encrypted_data = encrypt_data(Zeroizing::new(serialized), &extended_key);
+        let mut file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .truncate(true)
+            .open(PASSWORD_FILE)
+            .expect("Erreur lors de l'ouverture du fichier pour sauvegarde.");
+        file.write_all(encrypted_data.as_bytes()).expect("Erreur lors de l'écriture des données chiffrées dans le fichier.");
     }
 
-    fn load_passwords(master_password: &str) -> Self {
-        let extended_key = extend_key(master_password, 32);
+    fn load_passwords(master_password: &Zeroizing<String>) -> Self {
+        let extended_key = extend_key(&master_password, 32);
         if Path::new(PASSWORD_FILE).exists() {
-            let mut file = fs::File::open(PASSWORD_FILE).unwrap();
+            let mut file = fs::File::open(PASSWORD_FILE).expect("Erreur lors de l'ouverture du fichier de mots de passe.");
             let mut encrypted_data = String::new();
-            file.read_to_string(&mut encrypted_data).unwrap();
+            file.read_to_string(&mut encrypted_data).expect("Erreur lors de la lecture des données chiffrées.");
             match decrypt_data(encrypted_data, &extended_key) {
                 Ok(decrypted_data) => {
-                    serde_json::from_str(&decrypted_data).unwrap()
+                    serde_json::from_str(&decrypted_data).expect("Erreur lors de la désérialisation des données.")
                 },
                 Err(_) => {
                     println!("Erreur : Mot de passe maître incorrect.");
@@ -78,11 +84,11 @@ impl PasswordManager {
     }
 }
 
-fn encrypt_data(data: String, key: &str) -> String {
+fn encrypt_data(data: Zeroizing<String>, key: &str) -> String {
     let key = Key::from_slice(key.as_bytes());
     let cipher = Aes256Gcm::new(key);
     let nonce = Nonce::from_slice(NONCE);
-    let ciphertext = cipher.encrypt(nonce, data.as_ref()).expect("encryption failure!");
+    let ciphertext = cipher.encrypt(nonce, data.as_ref()).expect("Échec du chiffrement!");
     base64::encode(&ciphertext)
 }
 
@@ -90,15 +96,15 @@ fn decrypt_data(data: String, key: &str) -> Result<String, &'static str> {
     let key = Key::from_slice(key.as_bytes());
     let cipher = Aes256Gcm::new(key);
     let nonce = Nonce::from_slice(NONCE);
-    let ciphertext = base64::decode(&data).map_err(|_| "decoding failure")?;
-    let plaintext = cipher.decrypt(nonce, ciphertext.as_ref()).map_err(|_| "decryption failure")?;
-    String::from_utf8(plaintext).map_err(|_| "utf-8 conversion failure")
+    let ciphertext = base64::decode(&data).map_err(|_| "Échec du décodage")?;
+    let plaintext = cipher.decrypt(nonce, ciphertext.as_ref()).map_err(|_| "Échec du déchiffrement")?;
+    String::from_utf8(plaintext).map_err(|_| "Échec de la conversion en UTF-8")
 }
 
-fn extend_key(key: &str, length: usize) -> String {
+fn extend_key(key: &Zeroizing<String>, length: usize) -> String {
     let mut extended_key = key.to_string();
     while extended_key.len() < length {
-        extended_key.push_str(key);
+        extended_key.push_str(&key);
     }
     extended_key.truncate(length);
     extended_key
@@ -117,7 +123,7 @@ fn main() {
     }
 
     let command = &args[1];
-    let master_password = prompt_secure("Entrez le mot de passe maître : ");
+    let master_password = Zeroizing::new(prompt_secure("Entrez le mot de passe maître : "));
     let mut password_manager = PasswordManager::load_passwords(&master_password);
 
     match command.as_str() {
@@ -127,7 +133,7 @@ fn main() {
                 return;
             }
             let name = &args[2];
-            let password = prompt_secure("Entrez le mot de passe à ajouter : ");
+            let password = Zeroizing::new(prompt_secure("Entrez le mot de passe à ajouter : "));
             password_manager.add_password(name.to_string(), password, &master_password);
         },
         "get" => {
@@ -149,6 +155,6 @@ fn main() {
 
 fn prompt_secure(prompt: &str) -> String {
     print!("{}", prompt);
-    io::stdout().flush().unwrap();
-    read_password().unwrap()
+    io::stdout().flush().expect("Erreur lors du vidage du tampon de sortie.");
+    read_password().expect("Erreur lors de la lecture du mot de passe.")
 }
